5e1718a8fb16fe7da09041e7a8fbcfd48b5b7b7f
Ashutosh Tripathi <tripathiashutosh665@gmail.com>
2024-10-26 21:31:21 +0530

    List Add

--- collection_Interfaces/src/list/Program3.java
+++ collection_Interfaces/src/list/Program3.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class Program3 {
 	public static void main(String[] args) {
 		List l1=new ArrayList();
+		l1.add(34);
+		l1.add(1, 10);
+		System.out.println(l1);
+		System.out.println((Integer)l1.get(1)+100+(Integer)l1.get(0));
 	}
 
 }
